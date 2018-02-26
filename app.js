b1790583cf835f1b873412dc76b63f5424eb4b37
Yaspesberro <yaspesberro@gmail.com>
2018-02-26 15:01:43 -0800

    Deleting code from js

--- app.js
+++ app.js
@@ -1,19 +1,4 @@
 'use strict';
-var dog = [];
-var globalTimer=0;
-var timer=0;
-var cycle=0;
-var game=document.getElementById('game');
-var demo=document.getElementById('dogbark')
-var ctx=game.getContext('2d');
-// setInterval(incrementTimer,50);
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-// var imageObj = new Image();
-// imageObj.src = 'assests/KKona/dogBark0.png';
-// imageObj.onload= function(){
-//     ctx.drawImage();
-// }
-ctx.fillStyle='#3e94ff ';
-ctx.fillRect(0,0,game.width, game.height);
-ctx.fillStyle='#009e00 ';
-ctx.fillRect(0,350,game.width, game.height/3);
