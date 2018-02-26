fd0cae63a6cc13fff623229bbaa21c5640ff8c11
Sean Miller <smiller206@me.com>
2018-02-26 12:36:16 -0800

    starting point

--- app.js
+++ app.js
@@ -1,37 +1,19 @@
 'use strict';
+var dog = [];
 var globalTimer=0;
 var timer=0;
 var cycle=0;
 var game=document.getElementById('game');
 var demo=document.getElementById('dogbark')
 var ctx=game.getContext('2d');
-setInterval(incrementTimer,50);
+// setInterval(incrementTimer,50);
 
-function incrementTimer()
-{
-    Math.floor(timer+=1);
-    if(timer%4===0)
-    {
-        cycle+=1;
-    }
-    globalTimer
-    if(timer%3===0){
-    dogIdle();};
-//6 frame cycle of dog barking
-function dogIdle(){
-  if(timer%2===0)
-    {
-      var img=document.getElementById('bark0')
-      ctx.drawImage(img,10,10)
-      demo.src=('assets/KKona/dogBark0.png')
-      console.log(demo.src);
-  }
-  if(timer%2===1)
-    {
-      var img=document.getElementById('bark0')
-      ctx.drawImage(img,10,10)
-      demo.src=('assets/KKona/dogBark2.png')
-      console.log(demo.src);
-    }
-  }
-}
+// var imageObj = new Image();
+// imageObj.src = 'assests/KKona/dogBark0.png';
+// imageObj.onload= function(){
+//     ctx.drawImage();
+// }
+ctx.fillStyle='#3e94ff ';
+ctx.fillRect(0,0,game.width, game.height);
+ctx.fillStyle='#009e00 ';
+ctx.fillRect(0,350,game.width, game.height/3);
