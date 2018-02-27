5fe870859be970d62eb1c66ed7ab00588b8809a8
Sean Miller <smiller206@me.com>
2018-02-27 08:36:57 -0800

    moving dog

--- game.js
+++ game.js
@@ -1,35 +1,50 @@
 'usestrict';
-var obstacleTypes=new Array;
-var falling=false;
+var obstacleTypes=new Array; 
+var falling=false; 
 var obstacleGridConstructors = new Array;
 var stage=document.getElementById('stage');
 var ctx=stage.getContext('2d');
 console.log('working');
+//Background Image (this becomes invisible)
 ctx.fillStyle='#3e94ff';
 ctx.fillRect(0,0,stage.width,stage.height*.6666);
 ctx.fillStyle='#009e00';
 ctx.fillRect(0,stage.height*.6666,stage.width,stage.height/3);
+
+//State property to be added to Positioning State
 var state=
 {
   score:0,
 };
+
+//Refresh the screen
 function screenUpdate(){
   ctx.clearRect(0,0,stage.width,stage.height);
+
+  //Background Image (this becomes invisible)
   ctx.fillStyle='#3e94ff';
   ctx.fillRect(0,0,stage.width,stage.height*.6666);
   ctx.fillStyle='#009e00';
   ctx.fillRect(0,stage.height*.6666,stage.width,stage.height/3);
   ctx.fillStyle='#000000';
+
+  //Draws the Dog (this will be the Dog Image)
   playerCharacter.render();
   ctx.fillStyle='#000000';
+
+  //Test if Character hit an Obstacle
   testObstacle.collisionGridMaker;
   testObstacle.render();
 }
+
+//Create the grid and map the Canvas, will need for collision detection
+//this could be State Based
 function gridCreation(){
   this.gridWidth=stage.width/10;
   this.gridHeight=stage.height/10;
 }
 
+//Constructor for Character (this should be an Obj Literal)
 function Player(x,y,height,width){
   this.x=x;
   this.y=y;
@@ -46,6 +61,8 @@ function Player(x,y,height,width){
   };
 }
 
+//Obstacle consturcutor 
+//Grid properties could be State based
 function Obstacle(x,y,obstacleType){
   this.x=x;
   this.y=y;
@@ -55,25 +72,26 @@ function Obstacle(x,y,obstacleType){
   this.gridY=(this.y-80)/-10;
   this.gridX=this.x/10;
   this.collisionGrid=new Array;
-  console.log(obstacleTypes[obstacleType].collisiongridconstructor());
+  // console.log(obstacleTypes[obstacleType].collisiongridconstructor());
   this.movespeed=obstacleTypes[obstacleType].movespeed;
   this.render=function(){
     this.x-=this.movespeed;
     ctx.fillRect(this.x,this.y,this.height,this.width);
     this.gridY=(this.y-80)/-10;
     this.gridX=this.x/10;
-    console.log(obstacleTypes[obstacleType].collisiongridconstructor());
+    // console.log(obstacleTypes[obstacleType].collisiongridconstructor());
     if(this.gridY===playerCharacter.gridY&&this.gridX===playerCharacter.gridX)
     {
       console.log('You Lose!');
     }
     else if(this.gridX===playerCharacter.gridX){
-      console.log(this.pointValue);
+      // console.log(this.pointValue);
       state.score+=this.pointValue;
-      console.log(state.score);
+      // console.log(state.score);
     }
   };
 }
+//Function to take x & y and return them in array
 function coordinates(x,y)
 {
   return[x,y];
@@ -108,6 +126,7 @@ window.onkeydown=function(event){
     }}
   screenUpdate();
 };
+//Identify Which Constructors Describe
 obstacleGridConstructors.push(
   function()
   {
@@ -137,14 +156,15 @@ obstacleGridConstructors.push(
   }
 );
 //running .obstacle grid gives you an array of 2 integer arrays which are checked against other results from .obstacleGrid. There has to be a constructor which pulls from an array of these to assign them to each entity.
-function gameBoot(){gridCreation();
-//0 is mailbox, 1 is cat, 2 is rat and bird, 3 is box, 4 is raccoon.
+function gameBoot(){
+  gridCreation();
   new ObstacleTypeConstructor('mailbox',30,30,0,10,500);
   new ObstacleTypeConstructor('cat',20,10,1,30,250);
   new ObstacleTypeConstructor('rat',10,10,2,20,100);
   new ObstacleTypeConstructor('bird',10,10,2,20,100);
   new ObstacleTypeConstructor('box',20,20,3,10,100);
-  new ObstacleTypeConstructor('raccoon',10,20,4,20,300);}
+  new ObstacleTypeConstructor('raccoon',10,20,4,20,300);
+}
 function ObstacleTypeConstructor(name,width,height,collisiongrid,movespeed,pointValue){
   this.name=name;
   this.width=width;
