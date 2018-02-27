b6e540e550f79358f7472523cc313d7073551ded
Yaspesberro <yaspesberro@gmail.com>
2018-02-27 13:04:47 -0800

    adding form

--- form.js
+++ form.js
@@ -1,2 +1,21 @@
 'use strict';
-var form = getElementById('form');
+var name;
+var level;
+var form = document.getElementById('form');
+var formName = document.getElementById('name');
+var formSubmit = document.getElementById('submit');
+var formLevel = document.getElementById('levels');
+
+function levelHandler(e) {
+  console.log(e);
+}
+formLevel.addEventListener('change', levelHandler);
+
+function submitHandler(e) {
+  e.preventDefault();
+  var name = e.target.name.value;
+  var level = e.target.levels.value;
+  console.log(name);
+  console.log(level);
+}
+formSubmit.addEventListener('click', submitHandler);
