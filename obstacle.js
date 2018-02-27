26ad0e50a78cb7a526a27ba8aed8a3757db7121b
Sean Miller <smiller206@me.com>
2018-02-27 13:29:35 -0800

    working on obst show

--- obstacle.js
+++ obstacle.js
@@ -1,4 +1,40 @@
 'use strict';
-var obstacle=document.getElementById('obstacle');
-var ctx3=obstacle.getContext('2d');
+var object = document.getElementById('obstacles');
+var ctx3 = object.getContext('2d');
 
+var obstacles = [];
+function Obstacle(name, filePath, score){
+    var obstacleState = {
+        x : 1000,
+        y : 350,
+        // Hard code width by looking at image dimentions 
+    };
+    this.name = name;
+    this.filePath = filePath;
+    this.score = score
+    // this.renderImage();
+    obstacles.push(this);
+}
+// Obstacle.prototype.renderImage = function(){
+//     var tempImg = this.name;
+//     tempImg = new Image(10,5);
+//     tempImg.src = this.filePath;  
+// }
+Obstacle.prototype.drawObstacle= function(){
+    var tempImg = this.name;
+    tempImg = new Image(10,5);
+    tempImg.src = this.filePath;  
+    tempImg.onload = function(){
+        setInterval(function(){
+            ctx3.clearRect(0,0,900,500);
+            ctx3.drawImage(tempImg, this.obstacleState.x, this.obstacleState.y);
+            this.obstacleState.x--;    
+        },10)
+    }
+}
+var fireH = new Obstacle (fireH, 'img/fireHydrant.png', 15);
+//create rand num
+//render obj on screen at rand num 
+
+console.log(obstacles[0]);
+obstacles[0].drawObstacle();
