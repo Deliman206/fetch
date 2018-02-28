cc9e96c64eb31b0d65e2ed809aca7d2dc01d96ff
Yaspesberro <yaspesberro@gmail.com>
2018-02-28 09:49:47 -0800

    Adding functionality to frisbee

--- frisbee.js
+++ frisbee.js
@@ -8,29 +8,47 @@ function Frisbee() {
     x: 250,
     y: 0
   };
-  this.score = 0;
   this.image = new Image();
   this.image.src = 'img/fris_4.png';
 }
 
-Frisbee.prototype.launch = function(interval) {
-  setInterval(()=>{
-    //Starting launch coords
-    var xPos = 0;
-    var yPos = 250;
-
+Frisbee.prototype.launch = function() {
+  //Starting launch coords
+  this.state.x = 0;
+  this.state.y = 250;  
+  //Ending launch coords
+  var endX = 400;
+  var endY = -50;
+  var interval = setInterval(()=>{
     context.clearRect(0, 0, 900 ,500);
     context.drawImage(this.image, this.state.x, this.state.y, 60, 25);
-  }, interval);
+    if(this.state.x < endX) { this.state.x += 5; }
+    if(this.state.y > endY) { this.state.y -= 5; }
+    if(this.state.x >= endX && this.state.y <= endY) {
+      clearInterval(interval);
+    }
+  }, 5);
+
+
+
+  
 };
 
-Frisbee.prototype.draw = function(interval) {
-  setInterval(()=>{
-    context.clearRect(0, 0, 900 ,500);
+//Draws the frisbee at (x:400, y:0), slowly falls to ground
+Frisbee.prototype.draw = function() {
+  const ground = 400;
+  this.state.x = 400;
+  this.state.y = 0;
+
+  var interval = setInterval(()=>{
+    context.clearRect(0, 0, 900, 500);
     context.drawImage(this.image, this.state.x, this.state.y, 60, 25);
     this.state.y += 1;
-  }, interval);
+    //If frisbee has hit the ground = stop execution
+    if(this.state.y === ground) { clearInterval(interval); }
+  }, 10);
 };
 
-frisbeeObj.draw(50);
-
+frisbeeObj.launch();
+//setTimeout(frisbeeObj.draw(), 5);
+//frisbeeObj.draw();
