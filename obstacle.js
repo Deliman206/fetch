a52d70e111b325d244bbec141b920fb54a281420
Sean Miller <smiller206@me.com>
2018-02-27 18:54:30 -0800

    made random obstacles generate

--- obstacle.js
+++ obstacle.js
@@ -4,39 +4,59 @@ var ctx3 = object.getContext('2d');
 
 var obstacles = [];
 function Obstacle(name, filePath, score){
-    this.obstacleState = {
+    this.name = name;
+    this.filePath = filePath;
+    this.score = score
+    this.states= [];
+    this.offScreen = 0;
+    // this.resetState();
+    this.renderImage();
+    obstacles.push(this);
+}
+Obstacle.prototype.initialState = function(){
+   return {
         x : 1000,
         y : 350,
         // Hard code width by looking at image dimentions 
     };
-    this.name = name;
-    this.filePath = filePath;
-    this.score = score
-    this.renderImage();
-    obstacles.push(this);
 }
 Obstacle.prototype.renderImage = function(){
-    
-    this.name = new Image(10,5);
+    this.name = new Image();
     this.name.src = this.filePath;  
 }
-Obstacle.prototype.drawObstacle= function(interval){
-    // this.name.onload = function(){
-            setInterval(()=>{
-            ctx3.clearRect(0,0,900,500);
-            ctx3.drawImage(this.name, this.obstacleState.x, this.obstacleState.y);
-            this.obstacleState.x--;    
-        },interval)
-    // }
+Obstacle.prototype.drawObstacle= function(time){
+    var i = this.states.length;
+    this.states.push(this.initialState());
+    var interval = 
+        setInterval(()=>{
+        ctx3.clearRect(this.states[i].x,this.states[i].y,this.name.width+1,this.name.height);
+        ctx3.drawImage(this.name, this.states[i].x, this.states[i].y);
+        this.states[i].x--;    
+    },time)
+    return interval;
 }
 var fireH = new Obstacle ('fireH', 'img/fireHydrant.png', 15);
+var mailbox = new Obstacle('mailbox', 'img/sprites/mailbox.png', 10);
 //create rand num
 //render obj on screen at rand num 
-// var randomNum = //
+
 // function that does  this ===> obstacles[0].drawObstacle(5);
 //     but makes it random and shows up with space inbetween, so many obstacles on the screenLeft
 
+function renderRandomObstacle(){
+    var randomNum = Math.floor(Math.random()*obstacles.length);
+    console.log(randomNum);
+    var tempNum = randomNum;
+    var interval = obstacles[tempNum].drawObstacle(5);
+    setTimeout(function(){
+        clearInterval(interval);
+        obstacles[tempNum].offScreen++;
+    }, 8000);// How long it takes Obstacle to cross screen
+}
+(function(){
+    setInterval(renderRandomObstacle,2000)//How frequently Obstacle is generated
+})();
 
 
-// console.log(obstacles[0]);
-obstacles[0].drawObstacle(50);
+//collision detector will use iffScreen as index pointer
+//this is first index to check for collisions and then check to the end or length
