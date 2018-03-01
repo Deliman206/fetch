af6562921afec65e876ac4f4f3df7c5c71b2bec1
jackie <jcqnly@gmail.com>
2018-02-28 17:41:48 -0800

    work on background

--- background2.js
+++ background2.js
@@ -3,8 +3,8 @@ var carasel2=document.getElementById('background2');
 var ctx2=carasel2.getContext('2d');
 //State variable to control Relative Position
 var backgroundState2 ={
-    x: 1200,
-    y: 150
+  x: 800,
+  y: 150
 }
 ctx2.fillStyle='#3e94ff ';
 ctx2.fillRect(0,0,carasel2.width, carasel2.height);
@@ -16,23 +16,28 @@ var cloud = new Image(10,5);
 cloud.src = 'img/sprites/cloud.png';
 var bench = new Image(10,5);
 bench.src = 'img/sprites/bench.png';
+var slide = new Image(10,5);
+slide.src = 'img/sprites/slide.png';
 
 function drawTree(x,y){
-    tulips.onload = function(){
-        setInterval(function(){
-            ctx2.clearRect(0,0,900,500);
-            // ctx2.drawImage(pond, backgroundState2.x+600, backgroundState2.y-100);
-            for( var i =0; i<2; i++){
-                ctx2.drawImage(tulips, backgroundState2.x + 25, backgroundState2.y+180);
-                ctx2.drawImage(cloud, backgroundState2.x + 500, backgroundState2.y-100);
-                ctx2.drawImage(bench, backgroundState2.x + 250, backgroundState2.y+130);
-                if(backgroundState2.x< -600){
-                    backgroundState2.x = 1000;
-                }
-            }
-            backgroundState2.x--;    
-        },2)
-    }
+  tulips.onload = function(){
+    setInterval(function(){
+      ctx2.clearRect(0,0,900,500);
+      // ctx2.fillStyle='#3e94ff ';
+      // ctx2.fillRect(0,0,carasel2.width, carasel2.height);
+      // ctx2.fillStyle='#009e00 ';
+      // ctx2.fillRect(0,350,carasel2.width, carasel2.height/3);
+      for( var i =0; i<2; i++){
+        ctx2.drawImage(tulips, backgroundState2.x + 25, backgroundState2.y+180);
+        ctx2.drawImage(cloud, backgroundState2.x + 500, backgroundState2.y-100);
+        ctx2.drawImage(bench, backgroundState2.x + 250, backgroundState2.y+130);
+        if(backgroundState2.x< -600){
+          backgroundState2.x = 1000;
+        }
+      }
+      backgroundState2.x--;    
+    },2)
+  }
 }
 
 drawTree(backgroundState2.x,backgroundState2.y);
--- carasel.js
+++ carasel.js
@@ -3,8 +3,8 @@ var carasel=document.getElementById('background');
 var ctx1=carasel.getContext('2d');
 //State variable to control Relative Position
 var backgroundState ={
-    x: 800,
-    y: 150
+  x: 1100,
+  y: 150
 }
 ctx1.fillStyle='#3e94ff ';
 ctx1.fillRect(0,0,carasel.width, carasel.height);
@@ -16,28 +16,39 @@ var bush = new Image(10,5);
 bush.src = 'img/sprites/shrub.png';
 var lamp = new Image(10,5);
 lamp.src = 'img/sprites/lamp.png';
+var tulips = new Image(10,5);
+tulips.src = 'img/sprites/tulips.png';
+var cloud = new Image(10,5);
+cloud.src = 'img/sprites/cloud.png';
+var bench = new Image(10,5);
+bench.src = 'img/sprites/bench.png';
+var slide = new Image(10,5);
+slide.src = 'img/sprites/slide.png';
 
 function drawTree(x,y){
-    tree1.onload = function(){
-        setInterval(function(){
-            ctx1.clearRect(0,0,900,500);
-            ctx1.fillStyle='#3e94ff ';
-            ctx1.fillRect(0,0,carasel.width, carasel.height);
-            ctx1.fillStyle='#009e00 ';
-            ctx1.fillRect(0,350,carasel.width, carasel.height/3);
-            // ctx1.drawImage(tree1, backgroundState.x, backgroundState.y + 150);
-            ctx1.drawImage(bush, backgroundState.x -350, backgroundState.y +150);
-            ctx1.drawImage(lamp, backgroundState.x +300, backgroundState.y + 30);
-            // ctx1.drawImage(pond, backgroundState.x +250, backgroundState.y);
-            for( var i =0; i<1; i++){
-                ctx1.drawImage(tree1, backgroundState.x +400*i, backgroundState.y - 100);
-                if(backgroundState.x< -600){
-                    backgroundState.x = 1000;
-                }
-            }
-            backgroundState.x--;    
-        },2)
-    }
+  tree1.onload = function(){
+    setInterval(function(){
+      ctx1.clearRect(0,0,900,500);
+      ctx1.fillStyle='#3e94ff ';
+      ctx1.fillRect(0,0,carasel.width, carasel.height);
+      ctx1.fillStyle='#009e00 ';
+      ctx1.fillRect(0,350,carasel.width, carasel.height/3);
+      ctx1.drawImage(slide, backgroundState.x + 900, backgroundState.y +80);
+      ctx1.drawImage(tulips, backgroundState.x + 850, backgroundState.y +170);
+      ctx1.drawImage(bench, backgroundState.x + 710, backgroundState.y + 130);
+      ctx1.drawImage(cloud, backgroundState.x + 800, backgroundState.y - 150);
+      ctx1.drawImage(tulips, backgroundState.x + 650, backgroundState.y +170);
+      ctx1.drawImage(bush, backgroundState.x + 250, backgroundState.y +150);
+      ctx1.drawImage(lamp, backgroundState.x + 400, backgroundState.y + 30);
+      for( var i =0; i<1; i++){
+        ctx1.drawImage(tree1, backgroundState.x + 1000*i, backgroundState.y - 70);
+        if(backgroundState.x< -1100){
+          backgroundState.x = 900;
+        }
+      }
+      backgroundState.x--;    
+    },2)
+  }
 }
 
 drawTree(backgroundState.x,backgroundState.y);
