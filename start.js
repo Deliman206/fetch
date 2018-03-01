4b54e52667ba3886529f9fa94b7d3471ba087055
Yaspesberro <yaspesberro@gmail.com>
2018-03-01 09:29:04 -0800

    Cleaning up js file

--- start.js
+++ start.js
@@ -2,8 +2,7 @@
 var startDiv = document.getElementById('start-container');
 var startCanvas = document.getElementById('start-canvas');
 var startCtx = startCanvas.getContext('2d');
-//Global start game bool
-var gameStarted = false;
+
 //Creating start button image element
 var img = document.createElement('img');
 img.setAttribute('src', 'img/start-button.png');
@@ -12,23 +11,16 @@ startDiv.appendChild(img);
 startCtx.fillStyle = 'rgba(200, 200, 200, 0.75)';
 startCtx.fillRect(0, 0, startCanvas.width, startCanvas.height);
 
+//Start button press handler
 startDiv.addEventListener('click', function(e){
   //Checks if user clicked on start button
   if(e.clientX > 296 && e.clientX < 642 && e.clientY < 409 && e.clientY > 276) {
-    if(!gameStarted) { 
-      startDiv.style.visibility = 'hidden'; 
-      loadScripts();
-    }
-    console.log('div clicked');
+    startDiv.style.visibility = 'hidden'; 
+    loadScripts();
   }
-  //Start up game
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  alert('Game state: win/lose');
-}, false);
-
-
+//Loads js files fundament to game logic/states
 function loadScripts() {
   var script = document.createElement('script');
   script.src = "obstacle.js";
