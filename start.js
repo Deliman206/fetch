8bf750e2700e4edaa7608f27e261a6bf89b7a385
Yaspesberro <yaspesberro@gmail.com>
2018-03-01 07:54:36 -0800

    Making sure user clicks button to start

--- start.js
+++ start.js
@@ -10,8 +10,14 @@ startCtx.fillStyle = 'rgba(200, 200, 200, 0.75)';
 startCtx.fillRect(0, 0, startCanvas.width, startCanvas.height);
 
 startDiv.addEventListener('click', function(e){
-  console.log('div clicked');
-  startDiv.style.visibility = 'hidden';
-
+  if(e.clientX > 296 && e.clientX < 642
+  && e.clientY < 409 && e.clientY > 276) {
+    console.log('div clicked');
+    startDiv.style.visibility = 'hidden';
+  }
   //Start up game
 });
+function printMousePos(e) {
+  console.log('mouse click: X: ' + e.clientX + ', Y: ' + e.clientY);
+}
+document.addEventListener('click', printMousePos);
