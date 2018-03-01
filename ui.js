f9677103f2e063a42edca68885c9e3549677beb2
Panos Akio Kumasaka <panosrockbear@gmail.com>
2018-02-28 17:18:40 -0800

    workingu

--- ui.js
+++ ui.js
@@ -2,15 +2,13 @@
 var userInterface=document.getElementById('ui');
 var ctx1=userInterface.getContext('2d');
 ctx1.font='40pt';
-
 function renderScore(scoreIncrease)
 {
   ctx1.clearRect(0,0,userInterface.width,userInterface.height);
   ctx1.strokeText(gameState.score,0,10,100);
   if(scoreIncrease!==0)
   {
-  ctx1.strokeText(scoreIncrease,playerState.x+70,playerState.y);
-  setTimeout()
-  console.log(scoreIncrease);}
+    ctx1.strokeText(scoreIncrease,playerState.x+70,playerState.y);
+    console.log(scoreIncrease);}
   console.log(gameState.score);
 }
